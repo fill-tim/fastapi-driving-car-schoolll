36cecde58104e673b1600dcc99ffb84ab82f4b55
Fill <tima.filicheff1@yandex.ru>
2023-12-30 16:52:23 +0300

    upd

---
+++ tests/conftest.py
@@ -0,0 +1,51 @@
+from fastapi.testclient import TestClient
+from httpx import AsyncClient
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from app.core import settings
+import pytest
+import asyncio
+from typing import AsyncGenerator
+from sqlalchemy.orm import sessionmaker
+from app.main import app, data_base, Base
+
+
+engine_test = create_async_engine(settings.test_url)
+async_session_maker = sessionmaker(
+    engine_test, class_=AsyncSession, expire_on_commit=False
+)
+Base.metadata.bind = engine_test
+
+
+async def override_get_async_session() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session_maker() as session:
+        yield session
+
+
+app.dependency_overrides[data_base.session] = override_get_async_session
+
+
+@pytest.fixture(autouse=True, scope="session")
+async def prepare_database():
+    async with engine_test.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield
+    async with engine_test.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+
+
+# SETUP
+@pytest.fixture(scope="session")
+def event_loop(request):
+    """Create an instance of the default event loop for each test case."""
+    loop = asyncio.get_event_loop_policy().new_event_loop()
+    yield loop
+    loop.close()
+
+
+client = TestClient(app)
+
+
+@pytest.fixture(scope="session")
+async def ac() -> AsyncGenerator[AsyncClient, None]:
+    async with AsyncClient(app=app, base_url="http://test") as ac:
+        yield ac
